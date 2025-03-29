2f519575e14c505ee7b47761e6b002367d4b6b21
Karlis Berzins <berzinsh.karlis@gmail.com>
2025-03-29 15:40:06 +0200

    Initial commit

---
+++ MovieMate/MovieMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMateApp.swift
+//  MovieMate
+//
+//  Created by Karlis Berzins on 29/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
